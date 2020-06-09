64df897287754568fc21b5b3af579572ae659d2b
chende <972430746@qq.com>
2020-06-09 19:03:20 +0800

    upgrade library

--- src/Main/Home/http/Api.js
+++ src/Main/Home/http/Api.js
@@ -4,7 +4,7 @@ export const Api = {//非标准
     btcPrice: 'https://api.cryptonator.com/api/ticker/btc-usd',// 查询比特币价格
     filmsList: 'https://ghibliapi.herokuapp.com/films',//影片列表2
     queryCitiesAmount: 'http://www.webxml.com.cn/WebServices/MobileCodeWS.asmx/getDatabaseInfo',//查询各城市Mobile服务数量
-    queryAnimations: 'https://api.jikan.moe/v3/search/anime?q=Fate/Zero',//动漫列表
+    queryAnimations: 'https://api.jikan.moe/v3/search/anime',//动漫列表
     queryMembers: 'https://ghibliapi.herokuapp.com/people',//查询成员列表
 };
 
