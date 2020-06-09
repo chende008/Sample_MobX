ae5390f85d9f1a2b4bdfdcaef389a5963c5dd6bb
chende <972430746@qq.com>
2020-06-10 00:46:01 +0800

    upgrade library

--- src/Main/Welcome/LaunchController.js
+++ src/Main/Welcome/LaunchController.js
@@ -1,7 +1,7 @@
 import React, {PureComponent} from 'react';
 
 import AsyncStorage from '@react-native-community/async-storage';
-import {XStorage, XWidget} from 'react-native-easy-app';
+import {XStorage} from 'react-native-easy-app';
 import {RNStorage} from '../Common/storage/AppStorage';
 import {Actions} from 'react-native-router-flux'
 import RFLog from "../Common/utils/RFLog";
